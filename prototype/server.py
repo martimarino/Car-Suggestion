b10f1886f1dbbb219ce009934206bd06c64f2389
Roberta <r.matrella@studenti.unipi.it>
2023-03-29 17:40:33 +0200

    fix of server

--- prototype/server.py
+++ prototype/server.py
@@ -27,11 +27,15 @@ try:
                         # Read the length of the image as a 32-bit unsigned int. If the
                         # length is zero, quit the loop
                         image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+                        print(image_len)
                         if not image_len:
                                 client, addr = server_socket.accept()
-                                total = nr_positive / total_frames
+                                if total_frames == 0:
+                                        total = 0
+                                else:
+                                        total = nr_positive / total_frames
                                 print(total, nr_positive, total_frames, emotion)
-                                ack = str(math.trunc(total)*100)
+                                ack = str(round(total, 2)*100)
                                 client.send(ack.encode())
                                 break
                         # Construct a stream to hold the image data and read the image
